22f15a07b70a9a337ff8bc5edc0e2d3c3a50d443
Sandeep Koduri <skoduri@google.com>
2019-12-07 17:12:45 -0800

    removing unused library

--- server/main.py
+++ server/main.py
@@ -6,10 +6,6 @@ from google.appengine.api import users
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import ndb
 
-
-from lib import bottle
-from lib.bottle import abort, post, get, request, error, debug, redirect, response, static_file
-
 from sheets import get_data
 
 
