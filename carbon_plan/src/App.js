5115eba24ea8fbd85b636d4ce6d69687ea1eab77
Victoria Chu <victoria.p.chu@berkeley.edu>
2019-12-07 17:31:21 -0800

    missing jsx

--- carbon_plan/src/App.js
+++ carbon_plan/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Doughnut } from 'react-chartjs-2';
+import Dashboard from "./components/Dashboard.js"
 
 
 
@@ -7,13 +9,13 @@ export default class App extends React.Component {
 		super(props);
 		this.state = {people: [], safe_areas: [], not_safe_areas: [], responders: [], extra: []};
 	}
-
-	render() {
+  render() {
 	return (
 		<div >
 				<h1 style={{ paddingTop: "2em", textAlign: "center", color: "red" }}> P Cubed</h1>
 				<div style={{ paddingTop: "-2em", margin: "5em", width: "85vw", height: "50vh" }}>
-					
+        <Dashboard/>
+
 				</div>
 
 
