1fd0487b1ee32823870afae26c7f17d08afa7031
Sandeep Koduri <skoduri@google.com>
2019-12-07 17:11:37 -0800

    code for querying spreadsheet library and handler for api all

--- server/main.py
+++ server/main.py
@@ -10,14 +10,21 @@ from google.appengine.ext import ndb
 from lib import bottle
 from lib.bottle import abort, post, get, request, error, debug, redirect, response, static_file
 
-import source.rest
-import source.routes
+from sheets import get_data
 
 
-def main():
-    debug(True)
-    app = bottle.app()
-    run_wsgi_app(app)
+import webapp2
 
-if __name__ == "__main__":
-    main()
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp2 World!')
+
+class Api(webapp2.RequestHandler):
+    def get(self, name=''):
+        self.response.write(json.dumps(get_data(name)))
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    (r'/api/(.*)', Api),
+], debug=True)
