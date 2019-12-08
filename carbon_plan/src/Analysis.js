a9fb75415823d67890a9bee1ddf02fd53a6a579c
Linden Chiu <hchiu87@gmail.com>
2019-12-08 09:59:59 -0800

    build phase analysis

--- carbon_plan/src/Analysis.js
+++ carbon_plan/src/Analysis.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function Analysis() {
+   return (
+    <div className="project">
+      <h1>Breakdown</h1>
+    
+              <div className="phase_recommendations">
+                Recommendations
+              </div>
+              <div className="performance">
+                
+              </div>
+              <div className="anomalies">
+                Anomalies Detected
+              </div>
+   
 
+    </div>
+
+
+
+  );
 
 
 
